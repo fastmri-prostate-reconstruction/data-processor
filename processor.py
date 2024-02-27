48889ab1a93ef6f3d8ac1fc0d1a7863fb7958d36
zeynepdeniz <86568344+zeynepdenizgundogan@users.noreply.github.com>
2024-02-27 14:18:36 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -139,7 +139,7 @@ for split_name, split in zip(["train", "valid", "test"], [train_files, valid_fil
 
         shutil.rmtree('hf_cache')
         gc.collect()
-        break
+        #break
 
 upload_results.zip_folders(folders)
 upload_results.upload_folders(args.dataset_name, folders)
